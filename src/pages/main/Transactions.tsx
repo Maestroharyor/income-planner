672822d5ee6334f7433ae0bc7effbad6592e5d4b
maestroharyor <ayomide.odewale1@gmail.com>
2023-11-01 13:58:01 +0100

    Changes

--- src/pages/main/Transactions.tsx
+++ src/pages/main/Transactions.tsx
@@ -1,16 +1,16 @@
 import React, { Suspense } from "react";
-const TransactionsPageContainer = React.lazy(
-  () =>
-    import("../../components/partials/transactions/TransactionsPageContainer")
-);
+// const TransactionsPageContainer = React.lazy(
+//   () =>
+//     import("../../components/partials/transactions/TransactionsPageContainer")
+// );
 import PageLoader from "../../components/loaders/PageLoader";
 import { IonNav } from "@ionic/react";
-// import TransactionsPageContainer from "../../components/partials/transactions/TransactionsPageContainer";
+import TransactionsPageContainer from "../../components/partials/transactions/TransactionsPageContainer";
 
 const Transactions = () => {
   return (
     <Suspense fallback={<PageLoader />}>
-      <IonNav root={() => <TransactionsPageContainer />}></IonNav>
+      <TransactionsPageContainer />
     </Suspense>
   );
 };
